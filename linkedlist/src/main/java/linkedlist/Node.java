4159207d380d4481e48bcac1f65a9a76eb165bed
Yueying <wangyy395@gmail.com>
2021-05-04 11:44:47 -0400

    create node class and tests

--- linkedlist/src/main/java/linkedlist/Node.java
+++ linkedlist/src/main/java/linkedlist/Node.java
@@ -1,4 +1,19 @@
 package linkedlist;
 
 public class Node {
+
+    private Node next;
+    private int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
 }
